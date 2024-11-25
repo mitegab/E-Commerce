02517eadcde4c35df250af7d9514d5d074ff3b24
mitegab <104057957+mitegab@users.noreply.github.com>
2024-11-25 22:51:12 +0300

    Update Startup.cs
    ref:-another way of implementing the logic

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,14 +1,9 @@
 using API.Extensions;
-using API.Helpers;
 using API.Middleware;
-using Infrastructure.Data;
-using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using StackExchange.Redis;
 
 namespace API
 {
@@ -20,44 +15,22 @@ namespace API
             _config = config;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // ConfigureServices is streamlined to call extension methods
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper(typeof(MappingProfiles));
-            services.AddDbContext<StoreContext>(x => 
-                x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
-
-            services.AddDbContext<AppIdentityDbContext>(x => 
-                x.UseSqlServer(_config.GetConnectionString("IdentityConnection")));
-
-            services.AddSingleton<IConnectionMultiplexer>(c => {
-                var configuration = ConfigurationOptions.Parse(_config
-                    .GetConnectionString("Redis"),
-                true);
-                return ConnectionMultiplexer.Connect(configuration);
-            });
-
-            services.AddControllers();
-            services.AddApplicationServices();
+            services.AddApplicationServices(_config);
             services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
-                });
-            });
+            services.AddCorsPolicy();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Configure is streamlined to call extension methods
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
             app.UseStaticFiles();
 
@@ -67,7 +40,7 @@ namespace API
             app.UseAuthorization();
 
             app.UseSwaggerDocumentation();
-            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
